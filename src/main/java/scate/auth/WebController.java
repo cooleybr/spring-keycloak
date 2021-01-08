1b56021fa89a83caa196e5391e7860fe4ef89c8b
Brian R. Cooley <brian.r.cooley@baesystems.com>
2021-01-08 14:51:48 -0500

    initial commit

---
+++ src/main/java/scate/auth/WebController.java
@@ -0,0 +1,26 @@
+package scate.auth;
+
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class WebController {
+
+    @Autowired
+
+    @GetMapping(path = "/")
+    public String index() {
+        return "external";
+    }
+
+    @GetMapping(path = "/customers")
+    public String customers(Principal principal, Model model) {
+        return "customers";
+    }
+}
